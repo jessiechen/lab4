4a8d4fefbbc7b938520826eaad198f77a1c113c7
vagrant <vagrant@precise32.(none)>
2014-02-23 01:41:17 +0000

    add node.js

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,19 @@
  */
 
 exports.view = function(req, res){
-  res.render('index');
+  res.render('index',{
+  	'projects':[
+  	    {  	
+  	    	'name':'a thousand stars',
+  	        'image':'lorempixel.people.1.jpeg',
+        	'id':'project1'
+  	    },
+  	    {  	
+  	    	'name':"I'm here",
+  	        'image':'lorempixel.city.1.jpeg',
+        	'id':'project2'
+  	    },
+  	]
+
+  });
 };
